321316bdac060ac04c64f6d5d7ffb564fe408069
Kazuki-Takagi <kzkt0217@gmail.com>
2021-11-06 23:58:37 +0700

    3回目

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 import streamlit as st
-#import numpy as np
-#import pandas as pd
 import datetime as dt
 from PIL import Image
 
 st.title('มะนาว')
-st.write('まなお')
+st.write(' まなお ')
 
-Firstday = dt.date(year=2021, month=7, day=4)
+Zeroday = dt.date(year=2021, month=7, day=3)
 Today = dt.date.today()
-passed=Today-Firstday
+passed=Today-Zeroday
 
-"お迎えした日 : ", Firstday
-"お迎えしてから ", passed.days, "日たったよ！"
-#st.write(passed.strftime("お迎えしてから%Y年%mヶ月%d日"))
+"お迎えした日 : 2021年 7月 4日"
+"お迎えしてから今日で ", passed.days, "日目だよ！"
 
 img = Image.open('manao.jpeg')
 st.image(img, caption='มะนาว(まなお)氏 0歳',use_column_width=True)
 
-
-
-# df = pd.DataFrame(
-#     np.random.rand(100,2)/[50,50]+[35.69,139.70],
-#     columns=['lat','lon']
-# )
-# st.map(df)
