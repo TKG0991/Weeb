acb4256059e6aa41f0da7b64a6c1ed8973b01bfa
Kazuki-Takagi <kzkt0217@gmail.com>
2021-11-17 22:51:12 +0700

    4-4回目

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import datetime as dt
+import locale
 from PIL import Image
 from openpyxl import load_workbook
 
@@ -13,11 +14,13 @@ st.header('มะนาว')
 ###
 """
 
+locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
 Startday = dt.date(year=2021, month=7, day=3)
 Today = dt.date.today()
-passed=Today-Startday
+Whatday_full = Today.strftime('%A')
+passed = Today-Startday
 
-f" \n今日は {Today.strftime(' %Y年 %m月 %d日')} "
+f" \n今日は {Today.strftime(' %Y年 %m月 %d日')} （{Whatday_full}）"
 f" お迎えして {passed.days} 日目 ！"
 "    （ お迎えした日 : 2021年 7月 4日 ）"
 
@@ -31,6 +34,9 @@ df = pd.read_excel('ham_DB.xlsx',header=0, index_col=0)
 #今までのきょり
 Accum_Dist = df['メーター'].max()
 st.subheader(f'これまで {Accum_Dist} km 走ったよ ！ ')
+f"1日あたりの最高記録 {df['1日あたり'].max()} km"
+# (今後)　1日あたりの距離の移動平均の追加
+# (今後)　日付指定入力機能の追加
 
 if st.checkbox('記録する'):
     Ipt = st.number_input('今の メーター [km]',step=0.001,format="%.3f")
@@ -54,9 +60,9 @@ if st.checkbox('記録する'):
 """
 ###
 """
-"走った距離の推移"
+"走った距離の推移 (直近2週間)"
 #グラフ描画
-st.bar_chart(df['1日あたり'])
+st.bar_chart(df['1日あたり'].tail(14))
 """
 ###
 """
