93b723c38d2a4e8530fb02bdda362611684f6917
Kazuki-Takagi <kzkt0217@gmail.com>
2021-11-17 21:44:09 +0700

    4-3回目

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ Today = dt.date.today()
 passed=Today-Startday
 
 f" \n今日は {Today.strftime(' %Y年 %m月 %d日')} "
-f" お迎えして {passed.days} 日目 ！  （ お迎えした日 : 2021年 7月 4日 ）"
+f" お迎えして {passed.days} 日目 ！"
+"    （ お迎えした日 : 2021年 7月 4日 ）"
 
 img = Image.open('manao.jpeg')
 st.image(img, caption='まなちゃん',width=400)
@@ -29,7 +30,7 @@ df = pd.read_excel('ham_DB.xlsx',header=0, index_col=0)
 
 #今までのきょり
 Accum_Dist = df['メーター'].max()
-st.subheader(f'ここまで全部で {Accum_Dist} km 走ったよ ！ ')
+st.subheader(f'これまで {Accum_Dist} km 走ったよ ！ ')
 
 if st.checkbox('記録する'):
     Ipt = st.number_input('今の メーター [km]',step=0.001,format="%.3f")
@@ -37,7 +38,13 @@ if st.checkbox('記録する'):
     if st.button("登録！"):
         #記入日の1日前の日付を計算
         Yesterday = Today-dt.timedelta(days=1)
-        Whatday = Yesterday.strftime('%a')  
+        Whatday = Yesterday.strftime('%a')
+
+        #dfの'メーター'に一度ゼロを追加
+        df.loc[Yesterday.strftime('%Y/%m/%d')] = [0, 0, 0, 0]
+        #前回までの距離を再取得
+        Accum_Dist = df['メーター'].max()
+
         #1日の距離計算
         Day_Dist = Ipt - Accum_Dist
         #dfに追加
